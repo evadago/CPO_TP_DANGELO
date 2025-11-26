ea015f601e386f5ae9b22249842d43cca6008a74
Eva D'ANGELO <eva.dangelo@epfedu.fr>
2025-11-26 18:19:21 +0100

    TP_Quiz_DANGELO_26/11/2025

--- Interface_grafique/src/Question.java
+++ Interface_grafique/src/Question.java
@@ -18,10 +18,10 @@ public class Question {
 
     public Question(String intitule, String p1, String p2, String p3, String p4, int indexBonneReponse) {
         this.intitule = intitule;
-        this.p1 = p1;
-        this.p2 = p2;
-        this.p3 = p3;
-        this.p4 = p4;
+        this.P1 = P1;
+        this.P2 = P2;
+        this.P3 = P3;
+        this.P4 = P4;
         this.indexBonneReponse = indexBonneReponse;
         
         if (indexBonneReponse < 1) indexBonneReponse = 1 ;
