66ca0192e27e63ffac6574242f9f7953fbb02002
Eva D'ANGELO <eva.dangelo@epfedu.fr>
2025-12-06 21:52:32 +0100

    Mini_projet_cadenas_05/12/2025 Creation d'une classe combinaison pour developper un code secret de 4 chiffres

--- JavaApplication7/src/Combinaisons.java
+++ JavaApplication7/src/Combinaisons.java
@@ -2,13 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.util.Random;
 
 /**
  *
  * @author evada
  */
 public class Combinaisons {
-    int Chiffre;
+    private int[] chiffres = new int[4]; // tableau de 4 chiffres
+
+    public Combinaisons() {
+        Random rand = new Random();
+        for (int i = 0; i < chiffres.length; i++) {
+            chiffres[i] = rand.nextInt(10); // nombre entre 0 et 9
+        }
+    }
+
+    // Retourne la combinaison complète
+    public int[] getChiffres() {
+        return chiffres;
+    }
+
+    // Vérifie si une tentative correspond à la combinaison
+    public boolean verifier(int[] tentative) {
+        if (tentative.length != chiffres.length) return false;
+        for (int i = 0; i < chiffres.length; i++) {
+            if (tentative[i] != chiffres[i]) return false;
+        }
+        return true;
+    }
+
     
     
     
